ef74ec2109be00bfa31bcc79f83a0067a187566d
Abaza164 <yabaza71@gmail.com>
2023-11-24 15:46:02 +0200

    agent model done

---
+++ Models/agentModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const agentModel = new mongoose.Schema( 
+    {
+        agentId: {
+          type: String,
+          requierd:true
+        },
+        username: {
+          type: String,
+          required: true,
+          unique:true
+        },
+        password: {
+          type: String,
+          minLength: 5,
+          required: true,
+        },
+
+        email:{
+            type:String,
+            required:true
+
+        },
+
+        primaryCategory:{
+            type:String,
+            required:true
+        }
+      },
+      // schemaOptions
+      {
+        strict: true,
+        timestamps: true,
+      }
+    );
+
+ module.exports = mongoose.model('agentModel', agentModel);
+ module.exports.Schema = agentModel;   
