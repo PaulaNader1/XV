1e47ed5d0eca34ac864bb7df183072cb0c085565
PaulaNader1 <128845194+PaulaNader1@users.noreply.github.com>
2023-12-03 20:14:01 +0200

    usercontroller and userroutes edits

--- Backend/Routes/users.js
+++ Backend/Routes/users.js
@@ -38,3 +38,46 @@ router.get("/checkout/:id", authorizationMiddleware(['admin','customer']), userC
 //   .delete(userController.deleteUser);
 
 module.exports = router;
+
+// const authenticationMiddleware = require('../middleware/authenticationMiddleware');
+
+// User Registration
+// router.post('/register', userController.register);
+
+// User Login
+// router.post('/login', userController.login);
+
+// Get All Users
+// router.get('/users', authenticationMiddleware, authorizationMiddleware(['admin']), userController.getAllUsers);
+
+// Get User by ID
+router.get('/users/:id', authorizationMiddleware(['admin', 'user']), userController.getUser);
+
+// Update User Name
+router.put('/users/:id/update-username', authorizationMiddleware(['user']), userController.updateUserName);
+
+// Update Password
+router.put('/users/:id/update-password', authorizationMiddleware(['user']), userController.updatePassword);
+
+// Create Ticket
+router.post('/tickets/create', authorizationMiddleware(['user']), userController.createTicket);
+
+// Create Knowledge Base Entry
+//router.post('/knowledgebase/create', authenticationMiddleware, authorizationMiddleware(['admin']), userController.createKnowledgeBase);
+
+// Get All Knowledge Base Entries
+router.get('/knowledgebase', authorizationMiddleware(['user']), userController.getAllKnowledgeBase);
+
+// Get Knowledge Base Entries by Category
+router.get('/knowledgebase/category/:category', authorizationMiddleware(['user']), userController.getKnowledgeBaseByCategory);
+
+// Get Knowledge Base Entries by SubCategory
+router.get('/knowledgebase/subcategory/:subcategory', authorizationMiddleware(['user']), userController.getKnowledgeBaseBySubCategory);
+
+// Get Knowledge Base Entries by Title
+router.get('/knowledgebase/title', authorizationMiddleware(['user']), userController.getKnowledgeBaseBytitle);
+
+// Rate Ticket
+router.put('/tickets/:ticketId/rate', authorizationMiddleware(['user']), userController.rateTicket);
+
+module.exports = router;
--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -1,10 +1,9 @@
 const userModel = require("../Models/userModel");
 const ticketModel = require("../Models/ticketModel");
 const knowledgeBaseModel = require("../Models/knowledgeBaseModel");
-//const sessionModel = require("../Models/sessionModel");
 const jwt = require("jsonwebtoken");
 require('dotenv').config();
-const secretKey =process.env.SECRET_KEY ;
+const secretKey = process.env.SECRET_KEY;
 const bcrypt = require("bcrypt");
 const userController = {
   register: async (req, res) => {            // ready
@@ -37,6 +36,7 @@ const userController = {
       res.status(500).json({ message: "Server error" });
     }
   },
+
   login: async (req, res) => {
     try {
       const { email, password } = req.body;
@@ -59,24 +59,24 @@ const userController = {
       const expiresAt = new Date(+currentDateTime + 1800000); // expire in 3 minutes
       // Generate a JWT token
       const token = jwt.sign(
-        { user: { userId: user._id, role: user.role } },
+        { user: { userid: user.userid, role: user.role } },
         secretKey,
         {
           expiresIn: 3 * 60 * 60,
         }
       );
-      let newSession = new sessionModel({
-        userId: user._id,
-        token,
-        expiresAt: expiresAt,
-      });
-      await newSession.save();
+      // let newSession = new sessionModel({
+      //   userId: user._id,
+      //   token,
+      //   expiresAt: expiresAt,
+      // });
+      // await newSession.save();
       return res
         .cookie("token", token, {
           expires: expiresAt,
           withCredentials: true,
           httpOnly: false,
-          SameSite:'none'
+          SameSite: 'none'
         })
         .status(200)
         .json({ message: "login successfully", user });
@@ -85,6 +85,7 @@ const userController = {
       res.status(500).json({ message: "Server error" });
     }
   },
+
   getAllUsers: async (req, res) => {            // ready
     try {
       const users = await userModel.find();
@@ -93,6 +94,7 @@ const userController = {
       return res.status(500).json({ message: e.message });
     }
   },
+
   getUser: async (req, res) => {
     try {
       const user = await userModel.findById(req.params.id);
@@ -101,30 +103,41 @@ const userController = {
       return res.status(500).json({ message: error.message });
     }
   },
-  updateUser: async (req, res) => {
+
+  updateUserName: async (req, res) => {
     try {
       const user = await userModel.findByIdAndUpdate(
         req.params.id,
-        { name: req.body.name },
+        { username: req.body.username },
         {
           new: true,
         }
       );
-      return res.status(200).json({ user, msg: "User updated successfully" });
-    } catch (error) {
-      return res.status(500).json({ message: error.message });
-    }
-  },
-  deleteUser: async (req, res) => {
-    try {
-      const user = await userModel.findByIdAndDelete(req.params.id);
-      return res.status(200).json({ user, msg: "User deleted successfully" });
+      return res.status(200).json({ user, msg: "Username updated successfully" });
     } catch (error) {
       return res.status(500).json({ message: error.message });
     }
   },
 
-  createTicket: async (req, res) =>{
+  updatePassword: async (req, res) => {
+    try {
+      
+      const {password} = req.body.password;
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = await userModel.findByIdAndUpdate(
+        req.params.id,
+        { password: hashedPassword },
+        {
+          new: true,
+        }
+      );
+      return res.status(200).json({ user, msg: "Password updated successfully" });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  },
+
+  createTicket: async (req, res) => {
     try {
       const {
         userid,
@@ -133,7 +146,7 @@ const userController = {
         subCategory,
         priority,
       } = req.body;
-  
+
       // Create a new ticket
       const newTicket = new ticketModel({
         userid,
@@ -144,10 +157,10 @@ const userController = {
         date: new Date(),
         status: false, // Assuming a new ticket is initially not resolved
       });
-  
+
       // Save the ticket to the database
       await newTicket.save();
-  
+
       res.status(201).json({ message: "Ticket created successfully" });
     } catch (error) {
       console.error("Error creating ticket:", error);
@@ -155,19 +168,19 @@ const userController = {
     }
   },
 
-  createKnowledgeBase: async (req, res) =>{
+  createKnowledgeBase: async (req, res) => {
     try {
       const { title, category, subCategory, answer } = req.body;
-  
+
       const newKnowledgeBaseEntry = new knowledgeBaseModel({
         title,
         category,
         subCategory,
         answer,
       });
-  
+
       await newKnowledgeBaseEntry.save();
-  
+
       res.status(201).json({ message: "Knowledge Base entry created successfully" });
     } catch (error) {
       console.error("Error creating Knowledge Base entry:", error);
@@ -178,7 +191,7 @@ const userController = {
   getAllKnowledgeBase: async (req, res) => {
     try {
       const knowledgeBaseEntries = await knowledgeBaseModel.find();
-  
+
       res.status(200).json({ knowledgeBaseEntries });
     } catch (error) {
       console.error("Error getting Knowledge Base entries:", error);
@@ -190,7 +203,7 @@ const userController = {
     try {
       const { category } = req.params;
       const knowledgeBaseEntries = await knowledgeBaseModel.find({ category });
-  
+
       res.status(200).json({ knowledgeBaseEntries });
     } catch (error) {
       console.error("Error getting Knowledge Base entries by category:", error);
@@ -202,7 +215,7 @@ const userController = {
     try {
       const { subcategory } = req.params;
       const knowledgeBaseEntries = await knowledgeBaseModel.find({ subCategory: subcategory });
-  
+
       res.status(200).json({ knowledgeBaseEntries });
     } catch (error) {
       console.error("Error getting Knowledge Base entries by subcategory:", error);
@@ -212,9 +225,9 @@ const userController = {
 
   getKnowledgeBaseBytitle: async (req, res) => {
     try {
-      const { title } = req.params;
+      const { title } = req.body;
       const knowledgeBaseEntries = await knowledgeBaseModel.find({ title: title });
-  
+
       res.status(200).json({ knowledgeBaseEntries });
     } catch (error) {
       console.error("Error getting Knowledge Base entries by subcategory:", error);
@@ -225,68 +238,37 @@ const userController = {
   rateTicket: async (req, res) => {
 
     try {
-      const user = await userModel.findByIdAndUpdate(
-        req.params.id,
-        { name: req.body.name },
-        {
-          new: true,
-        }
+      const { ticketId } = req.params;
+      const { responserating } = req.body;
+
+      // Validate the rating (assuming a rating between 1 and 5)
+      if (responserating < 0 || responserating > 5) {
+        return res.status(400).json({ message: "Invalid rating. Please provide a rating between 0 and 5" });
+      }
+
+      // Check if the ticket exists
+      if (!updatedTicket) {
+        return res.status(404).json({ message: "Ticket not found" });
+      }
+
+      // Check if the ticket has already been rated
+      if (updatedTicket.responserating !== null) {
+        return res.status(400).json({ message: "Ticket has already been rated" });
+      }
+
+      // Update the ticket with the rating using findByIdAndUpdate
+      const updatedTicket = await ticketModel.findByIdAndUpdate(
+        ticketId,
+        { responserating: { responserating } },
+        { new: true } // Returns the updated document
       );
-      return res.status(200).json({ user, msg: "User updated successfully" });
+
+      res.status(200).json({ message: "Ticket rated successfully", ticket: updatedTicket });
     } catch (error) {
-      return res.status(500).json({ message: error.message });
+      console.error("Error rating ticket:", error);
+      res.status(500).json({ message: "Server error" });
     }
   },
 
-  // getShoppingCart: async (req, res) => {
-  //   try {
-  //     const user = await userModel.findById(req.params.id);
-  //     return res.status(200).json(user.shoppingCart);
-  //   } catch (error) {
-  //     return res.status(500).json({ message: error.message });
-  //   }
-  // },
-  // addToCart: async (req, res) => {
-  //   try {
-  //     const user = await userModel.findById(req.params.id);
-  //     const product = await productModel.findById(req.params.productid);
-  //     user.shoppingCart.push(product);
-  //     const newUser = await user.save(); // save here works as update
-  //     return res.status(201).json(newUser);
-  //   } catch (e) {
-  //     return res.status(400).json({ message: e.message });
-  //   }
-  // },
-  // removeFromCart: async (req, res) => {
-  //   try {
-  //     const user = await userModel.findById(req.params.id);
-  //     const product = await productModel.findById(req.params.productid);
-  //     user.shoppingCart.pull(product);
-  //     const newUser = await user.save();
-  //     return res.status(201).json(newUser);
-  //   } catch (e) {
-  //     return res.status(400).json({ message: e.message });
-  //   }
-  // },
-  // checkout: async (req, res) => {
-  //   try {
-  //     const user = await userModel.findById(req.params.id);
-  //     const total = user.shoppingCart.reduce(
-  //       (total, product) => total + product.price,
-  //       0
-  //     );
-  //     await userModel.findByIdAndUpdate(
-  //       req.params.id,
-  //       { shoppingCart: [] },
-  //       {
-  //         new: true,
-  //       }
-  //     );
-  //     console.log(total);
-  //     return res.status(200).json(total);
-  //   } catch (error) {
-  //     return res.status(500).json({ message: error.message });
-  //   }
-  // },
 };
 module.exports = userController;
