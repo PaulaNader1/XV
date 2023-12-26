8cde9451a18951e7c597fade24af2ca2cc447b0c
GehadAhmed4 <gehad22004@gmail.com>
2023-12-26 17:21:20 +0200

    manager edits all done

--- Backend/Routes/managerRoutes.js
+++ Backend/Routes/managerRoutes.js
@@ -4,17 +4,13 @@ const managerController = require('../controller/managerController');
 const adminController = require('../controller/adminController');
 const authorizationMiddleware = require("../Middleware/authorizationMiddleware");
 
-// Route to get all tickets
-router.get('/tickets', managerController.getAllTickets);
+router.get('/tickets', authorizationMiddleware(['manager']), managerController.getAllTickets);
 
-// Route to get a specific ticket by ticketId
 router.get('/tickets/:ticketId',  authorizationMiddleware(['manager']), managerController.getTicketById);
 
 router.get("/ticket-Analytics",  authorizationMiddleware(['manager']), managerController.ticketAnalytics);
 
 router.get("/generateReports/:id", authorizationMiddleware(['manager']), managerController.generateReports);
-// router.get("/ticket/:id/generate-Reports",  authorizationMiddleware(['manager']), managerController.generateReports);
-// router.get('/generateReports/:ticketid', managerController.generateReports);
 
 
 module.exports = router;
