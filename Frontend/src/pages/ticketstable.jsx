05705c1c1915fe09777bcf5c820debbfec2a6371
PaulaNader1 <128845194+PaulaNader1@users.noreply.github.com>
2024-01-06 18:31:09 +0200

    .

--- Frontend/src/pages/ticketstable.jsx
+++ Frontend/src/pages/ticketstable.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import AppNavBar from '../components/navbar';
 import '../stylesheets/TicketsTable.css';
 
 const bearerHeader = {
